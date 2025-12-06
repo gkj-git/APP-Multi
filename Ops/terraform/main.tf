394cc080e4a236bbbd542204fe0e56d4c812e223
gkj-git <eaddress.gopi@gmail.com>
2025-12-06 16:01:48 -0500

    Updating EKS

--- Ops/terraform/main.tf
+++ Ops/terraform/main.tf
@@ -12,41 +12,55 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = ">= 4.0.0"
+
   name = "${var.cluster_name}-vpc"
   cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+
   public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
   private_subnets = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
+
   enable_nat_gateway = true
   single_nat_gateway = true
 }
 
-data "aws_availability_zones" "available" {}
-
 resource "aws_ecr_repository" "app" {
   name = var.ecr_repo_name
+
   image_tag_mutability = "MUTABLE"
-  image_scanning_configuration { scan_on_push = true }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = ">= 18.0.0"
+
   cluster_name = var.cluster_name
   cluster_version = "1.27"
-  vpc_id = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets  # FIXED
+
   node_groups = {
     default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_types   = ["t3.medium"]
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
+
+      instance_types = ["t3.small"]
     }
   }
+
   tags = {
     Owner = "devops-team"
   }
