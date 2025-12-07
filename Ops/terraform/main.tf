f628c8dfe34db0ad7048def17306707185c98e99
gkj-git <eaddress.gopi@gmail.com>
2025-12-06 19:52:40 -0500

    changed all file and developing new

--- Ops/terraform/main.tf
+++ Ops/terraform/main.tf
@@ -1,79 +1,92 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    aws ={
+        source = "hashicorp/aws"
+        version = "~> 5.0"
     }
   }
-  required_version = ">= 1.4.0"
 }
 
 provider "aws" {
-  region = var.aws_region
+   region = "ca-central-1"
 }
 
-data "aws_availability_zones" "available" {
-  state = "available"
+
+resource "aws_iam_role" "test_role" {
+  name = "Terraform-GKJ"
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17"
+    "Statement": [
+     {
+       "Effect" "Allow",
+       "Prinicipal":{
+           "Service": "ec2.amazonaws.com"
+           },
+           "Action": "sts:Assumerole"
+     }
+    ]
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 4.0.0"
-
-  name = "${var.cluster_name}-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
-  private_subnets = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
-resource "aws_ecr_repository" "app" {
-  name = var.ecr_repo_name
-
-  image_tag_mutability = "MUTABLE"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = ">= 18.0.0"
-
-  cluster_name = var.cluster_name
-  cluster_version = "1.27"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets  # FIXED
-
-  node_groups = {
-    default = {
-      desired_size = 2
-      max_size     = 3
-      min_size     = 1
-
-      instance_types = ["t3.small"]
+EOF
+    
     }
-  }
 
-  tags = {
-    Owner = "devops-team"
-  }
+resource "aws_iam_role_policy_attachment" "test_attachment" {
+    role = aws_iam_role.test_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
-output "ecr_repository_url" {
-  value = aws_ecr_repository.app.repository_url
+resource "aws_iam_instance_profile" "test_role" {
+    name = "Terraform-GKJ"
+  
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+resource "aws_security_group" "Jenkins-sg" {
+  name = "Jenkins-securitygroup"
+  description = "open 22,443,80,8080,9000,3000"
+
+
+#This single ingress rule will allow traffic for all specified port
+  ingress = [
+    for port in [22,443,80,8080,9000,3000]: {
+        description = "TLS from VPC"
+        from_port = port
+        to_port = port
+        protcol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = []
+        prefix_list_ids = []
+        security_groups = []
+        self = flase
+
+    }
+  ]
+   egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   tags = {
+    Name = "Jenkins-sg"
+   }
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_id
-}
+   resource "aws_instance" "app"{
+    ami = "ami-0abac8735a38475db"
+    instance_type = "t3-medium"
+    key_name = "mac"
+    vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
+    user_data = templatefile("./install_tools.sh",{})
+    iam_instance_profile = aws_iam_instance_profile.test_role.name
+
+    tags ={
+        Name = "Jenkins-argo"
+    }
+
+    root_block_device {
+        volume_size = 30 
+    }
+   }
